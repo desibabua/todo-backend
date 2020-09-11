f35be0cbca96b7890623282bc12fedf28f1e50c9
ayush <58028488+desibabua@users.noreply.github.com>
2020-09-11 18:39:51 +0530

    added redis cloud url

--- src/app.js
+++ src/app.js
@@ -3,10 +3,19 @@ const redis = require('redis');
 const {DbClient} = require('./dbClient');
 const handlers = require('./handlers');
 
-const client = redis.createClient({
-  url: 'redis://127.0.0.1:6379',
-  db: 1,
-});
+// const client = redis.createClient({
+  // url: 'redis://127.0.0.1:6379',
+  // db: 1,
+// });
+
+const getRedisClient = function () {
+  if (process.env.REDISCLOUD_URL) {
+    return redis.createClient(process.env.REDISCLOUD_URL, {
+      no_ready_check: true,
+    });
+  }
+  return redis.createClient();
+};
 
 const app = express();
 app.use(express.json());
@@ -21,7 +30,7 @@ app.get('/', (req, res) => {
   res.sendFile('index.html');
 });
 
-app.locals.db = new DbClient(client);
+app.locals.db = new DbClient(getRedisClient());
 
 app.get('/api/getAllToDos', handlers.getAllToDos);
 
